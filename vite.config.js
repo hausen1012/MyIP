43059c81316c1d735f6d10962757b1108ff88703
hausen1012 <50026605+hausen1012@users.noreply.github.com>
2024-05-10 00:18:02 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
       }
     }),
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: 'none',
       injectRegister: 'auto',
       workbox: {
         globPatterns: [
